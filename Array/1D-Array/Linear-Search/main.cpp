edea46109ad94974bbfed8f472b95da9903d1d14
Shubhajit Basak <shubhajitbasak45@gmail.com>
2025-01-25 17:58:29 +0530

    Update main.cpp

--- Array/1D-Array/Linear-Search/main.cpp
+++ Array/1D-Array/Linear-Search/main.cpp
@@ -1,28 +1,25 @@
-
-// Linear Searching----------------------------------->>>>>
-
-#include <iostream>
+// Linear Searching-------------------------------------------
+#include<iostream>
+#include<math.h>
+#include<limits.h>
 using namespace std;
 
-bool targetElement(int arr[], int size, int target)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == target)
-        {
+// Find The Target Element----------
+bool targetElement(int arr[], int size, int target){
+    for(int i=0 ; i<size ; i++){
+        if(arr[i]==target){
             return true;
         }
     }
     return false;
 }
 
-int index_of_targetElement(int arr[], int size, int target)
-{
-    int index = -1;
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == target)
-        {
+
+// Find Index of an Target Element---------
+int index_of_targetElement(int arr[], int size , int target){
+    int index=-1 ;
+    for(int i=0 ; i<size ; i++){
+        if(arr[i]==target){
             index = i;
             break;
         }
@@ -30,37 +27,118 @@ int index_of_targetElement(int arr[], int size, int target)
     return index;
 }
 
-int main()
-{
+// Find Max_Element of an Array---------
+int findMax_Element(int arr[], int size){
+    int ans = INT_MIN;
+    for(int i=0 ; i<size ; i++){
+        if(arr[i]>ans){
+            ans = arr[i];
+        }
+    }
+    return ans;
+}
 
-    // Find The Target Element----------
+
+// count 0's/1's----------------
+void count(int arr[], int size){
+    int ZeroesCount =0;
+    int OneCount=0;
+    for(int i =0 ; i<size ; i++){
+        if(arr[i]==0){
+            ZeroesCount++;
+        }else if(arr[i]==1){
+            OneCount++;
+        }
+    }
+    cout<<"Total Zeroes: "<<ZeroesCount<<endl;
+    cout<<"Total One: "<<OneCount<<endl;
+    
+}
+
+
+// ExtremePrinting----------------
+void ExtremePrinting(int arr[], int size){
+    int i=0;
+    int j= size-1;
+    while(i<=j){
+        if(i==j){
+            cout<<arr[i]<<" ";
+            break;
+        }
+        else{
+            cout<<arr[i]<<" ";
+            i++;
+            cout<<arr[j]<<" ";
+            j--;
+        }
+    }
+}
+
+// *********Reversed Array----------------
+void ReverseArray(int arr[], int size){
+    int i=0;
+    int j =size-1;
+    while(i<j){
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+}
+
+// FLips 0 to 1 And 1 to 0------------
+void Flips(int arr[], int size){
+    for(int i=0 ; i<size ; i++){
+        if(arr[i]==0){
+            arr[i]=1;
+        }
+        else if(arr[i]==1){
+            arr[i]=0;
+        }
+    }
+}
+
+
+
+
+int main(){
+
+  
 
     int size;
-    cout << "Enter the size of an array: " << endl;
-    cin >> size;
+    cout<<"Enter the size of an array: "<<endl;
+    cin>>size;
 
     int arr[100];
-    cout << "Enter The element of an array: " << endl;
-    for (int i = 0; i < size; i++)
-    {
-        cin >> arr[i];
+    cout<<"Enter The element of an array: "<<endl;
+    for(int i=0 ; i<size ; i++){
+        cin>>arr[i];
     }
 
     int target_Element;
-    cout << "Enter the target element: " << endl;
-    cin >> target_Element;
+    cout<<"Enter the target element: "<<endl;
+    cin>>target_Element;
 
-    bool final_ans = targetElement(arr, size, target_Element);
-    cout << final_ans;
+    bool final_ans = targetElement(arr,size,target_Element);
+    cout<<final_ans;
 
-    int final_index = index_of_targetElement(arr, size, target_Element);
-    if (final_index != -1)
-    {
-        cout << "Element Found at:  " << final_index;
+    int final_index = index_of_targetElement(arr, size , target_Element);
+    if(final_index != -1){
+        cout<<"Element Found at:  "<<final_index;
+    }else{
+        cout<<"Element Not found"<<endl;
     }
-    else
+
+
+    int Max = findMax_Element(arr,size);
+    cout<<Max;
+
+    count(arr,size);
+    ExtremePrinting(arr, size);
+    ReverseArray(arr,size);
+    Flips(arr, size);
+    for (int i = 0; i <size; i++)
     {
-        cout << "Element Not found" << endl;
+        cout<<arr[i]<<" ";
     }
     
     return 0;
